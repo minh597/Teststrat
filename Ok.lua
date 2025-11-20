e74039a610af7bbe430d16926cfde843f3fbeed4
minh597 <minhdangtran1012@gmail.com>
2025-11-20 08:34:28 +0700

    Update Ok.lua

--- Ok.lua
+++ Ok.lua
@@ -19,9 +19,11 @@ local remoteEvent = ReplicatedStorage:WaitForChild("RemoteEvent")
 local vu = game:GetService("VirtualUser")
 
 player.Idled:Connect(function()
-    vu:Button2Down(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
-    task.wait(1)
-    vu:Button2Up(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
+    if vu then
+        vu:Button2Down(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
+        task.wait(1)
+        vu:Button2Up(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
+    end
 end)
 
 local skipVotingFlag = false
